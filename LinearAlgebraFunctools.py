81a0017dadc1c891504ab0d73ce96d9f6faee9e2
Ishan Dandekar <ishandandekar2002@gmail.com>
2022-06-01 10:02:36 +0530

    Add more functions

--- LinearAlgebraFunctools.py
+++ LinearAlgebraFunctools.py
@@ -77,3 +77,144 @@ def gen_transpose(matrix):
             transpose[j][i] = matrix[i][j]
 
     return transpose
+
+
+def add_matrix(A, B):
+    """Adds two matrices
+
+    Args:
+        A (list): list representing a matrix
+        B (list): list representing a matrix
+
+    Raises:
+        ArithmeticError: Shape of matrix A and B should match
+
+    Returns:
+        list: representing addition of matrices
+    """
+    rows_a = len(A)
+    cols_a = len(A[0])
+    rows_b = len(B)
+    cols_b = len(B[0])
+    if rows_a == rows_b and cols_a == cols_b:
+        matrix = gen_zeros(rows_a, cols_a)
+        for i in range(rows_a):
+            for j in range(cols_a):
+                matrix[i][j] = A[i][j]+B[i][j]
+        return matrix
+    else:
+        raise ArithmeticError("Shape of matrices don't match!")
+
+
+def subtract_matrix(A, B):
+    """Subtracts two matrices
+
+    Args:
+        A (list): list representing a matrix
+        B (list): list representing a matrix
+
+    Raises:
+        ArithmeticError: Shape of matrix A and B should match
+
+    Returns:
+        list: representing subtraction of matrices
+    """
+    rows_a = len(A)
+    cols_a = len(A[0])
+    rows_b = len(B)
+    cols_b = len(B[0])
+    if rows_a == rows_b and cols_a == cols_b:
+        matrix = gen_zeros(rows_a, cols_a)
+        for i in range(rows_a):
+            for j in range(cols_a):
+                matrix[i][j] = A[i][j]-B[i][j]
+        return matrix
+    else:
+        raise ArithmeticError("Shape of matrices don't match!")
+
+
+def matrix_multiply(A, B):
+    """Multiplies two matrices
+
+    Args:
+        A (list): representing a matrix
+        B (list): representing a matrix
+
+    Raises:
+        ArithmeticError: Shapes of matrices should be same
+
+    Returns:
+        list: representing a matrix
+    """
+    rowsA = len(A)
+    colsA = len(A[0])
+    rowsB = len(B)
+    colsB = len(B[0])
+    if colsA != rowsB:
+        raise ArithmeticError(
+            'Number of A columns must equal number of B rows.')
+    C = gen_zeros(rowsA, colsB)
+    for i in range(rowsA):
+        for j in range(colsB):
+            total = 0
+            for ii in range(colsA):
+                total += A[i][ii] * B[ii][j]
+            C[i][j] = total
+
+    return C
+
+
+def is_equal(A, B, tol: int = None):
+    """Checks if two matrices are equal
+
+    Args:
+        A (list): representing a matrix
+        B (list): representing a matrix
+        tol (int, optional): tolerance of numbers in the matrix. Defaults to None.
+
+    Returns:
+        bool: A boolean value representing if the matrices are equal or not
+    """
+    if len(A) != len(B) or len(A[0]) != len(B[0]):
+        return False
+
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            if tol is None:
+                if A[i][j] != B[i][j]:
+                    return False
+            else:
+                if round(A[i][j], tol) != round(B[i][j], tol):
+                    return False
+
+    return True
+
+
+def dot_product(A, B):
+    rowsA = len(A)
+    colsA = len(A[0])
+    rowsB = len(B)
+    colsB = len(B[0])
+    if rowsA != rowsB or colsA != colsB:
+        raise ArithmeticError('Matrices are NOT the same size.')
+
+    result = 0
+    for i in range(rowsA):
+        for j in range(colsB):
+            result += A[i][j] * B[i][j]
+
+    return result
+
+
+def is_square(matrix):
+    """Checks if the matrix is a square matrix
+
+    Args:
+        matrix (list): representing a matrix
+
+    Returns:
+        bool: represents the result of the condition
+    """
+    if len(matrix) == len(matrix[0]):
+        return True
+    return False
